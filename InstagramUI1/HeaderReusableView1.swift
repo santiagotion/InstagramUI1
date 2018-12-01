53aa92d979001369849f2da108cd683f9cdf00da
Santi Robeiro <santiagotion@gmail.com>
2018-12-01 23:44:13 +0000

    BugFixing

--- InstagramUI1/HeaderReusableView1.swift
+++ InstagramUI1/HeaderReusableView1.swift
@@ -8,6 +8,26 @@
 
 import UIKit
 
-class HeaderReusableView1: UICollectionReusableView {
+class HeaderReusableView1: UICollectionReusableView, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
         
+        collectionview.delegate = self
+        collectionview.dataSource = self
+        
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SuggestionsCell3", for: indexPath) as! SuggestionsCell
+        return cell
+    }
+    
+    @IBOutlet weak var collectionview: UICollectionView!
+    
 }
--- InstagramUI1/Profillll2222VVCC.swift
+++ InstagramUI1/Profillll2222VVCC.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class Profillll2222VVCC: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var jhu: NSLayoutConstraint!
+    let data = DataSet()
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 60
+        return data.categories2.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "koko", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "koko", for: indexPath) as! FeedCell
+        cell.configureCell(category: data.categories2[indexPath.row])
         return cell
     }
     
@@ -25,17 +35,62 @@ class Profillll2222VVCC: UIViewController, UICollectionViewDataSource, UICollect
     }
 
     
+    @IBAction func ho(_ sender: Any) {
+let layout = UICollectionViewFlowLayout()
+       
+        let itemSize = UIScreen.main.bounds.width/3 - 2
+        
+        layout.sectionInset = UIEdgeInsets(top: 1, left: 0, bottom: 3, right: 0)
+        layout.itemSize = CGSize(width: itemSize, height: itemSize)
+        layout.minimumLineSpacing = 1
+        layout.minimumInteritemSpacing = 1
+         layout.headerReferenceSize = CGSize(width: view.frame.width, height: 450)
+        collectionView.collectionViewLayout = layout
+    }
     
     
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        
+        let itemSize = UIScreen.main.bounds.width/3 - 2
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 1, left: 0, bottom: 3, right: 0)
+        layout.itemSize = CGSize(width: itemSize, height: itemSize)
+        layout.minimumLineSpacing = 1
+        layout.minimumInteritemSpacing = 1
+        layout.headerReferenceSize = CGSize(width: view.frame.width, height: 206)
+        
+        
+        
+        
+        collectionView.collectionViewLayout = layout
 
-        // Do any additional setup after loading the view.
+        
     }
     
 
    
 
 }
+//extension Profillll2222VVCC: UICollectionViewDelegateFlowLayout {
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+//        return 1
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//        return 1
+//    }
+//
+//
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+//        return CGSize(width: view.frame.width, height: 206)
+//    }
+//}
